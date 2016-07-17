0606f50e231339c107506a2fd01e149ec91fa1a9
Kevin Burleigh <klb@kindling>
2016-07-17 16:11:09 -0500

    adjust some indentation

--- ruby/lib/ugly_trivia/game.rb
+++ ruby/lib/ugly_trivia/game.rb
@@ -87,22 +87,22 @@ module UglyTrivia
     end
 
     def ask_question
-      puts @pop_questions.shift if current_category == 'Pop'
+      puts @pop_questions.shift     if current_category == 'Pop'
       puts @science_questions.shift if current_category == 'Science'
-      puts @sports_questions.shift if current_category == 'Sports'
-      puts @rock_questions.shift if current_category == 'Rock'
+      puts @sports_questions.shift  if current_category == 'Sports'
+      puts @rock_questions.shift    if current_category == 'Rock'
     end
 
     def current_category
-      return 'Pop' if @places[@current_player] == 0
-      return 'Pop' if @places[@current_player] == 4
-      return 'Pop' if @places[@current_player] == 8
+      return 'Pop'     if @places[@current_player] == 0
+      return 'Pop'     if @places[@current_player] == 4
+      return 'Pop'     if @places[@current_player] == 8
       return 'Science' if @places[@current_player] == 1
       return 'Science' if @places[@current_player] == 5
       return 'Science' if @places[@current_player] == 9
-      return 'Sports' if @places[@current_player] == 2
-      return 'Sports' if @places[@current_player] == 6
-      return 'Sports' if @places[@current_player] == 10
+      return 'Sports'  if @places[@current_player] == 2
+      return 'Sports'  if @places[@current_player] == 6
+      return 'Sports'  if @places[@current_player] == 10
       return 'Rock'
     end
 
